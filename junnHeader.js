c040a246b5ce500d141f8fc87eab7fa2b221054a
junn-dev <hungnguyen.junn@gmail.com>
2025-09-11 02:48:25 +0700

    Update junnHeader.js

--- junnHeader.js
+++ junnHeader.js
@@ -1,28 +1,8 @@
 /***********************************************
- > deleteHeader by Junn.k6
- > Version: V1.0.2
-***********************************************/
+> deleteHeader by Junn.k6
+***********************************************/	
 
-const version = "V1.0.2";
+const version = 'V1.0.2';
 
-/**
- * Hàm setHeaderValue
- * - Nếu header đã tồn tại (case-insensitive) → cập nhật giá trị
- * - Nếu chưa tồn tại → thêm mới
- */
-function setHeaderValue(headers, key, value) {
-  const lowerKey = key.toLowerCase();
-  if (lowerKey in headers) {
-    headers[lowerKey] = value;
-  } else {
-    headers[key] = value;
-  }
-}
 
-let modifiedHeaders = $request.headers;
-
-// Xoá giá trị ETag (ngăn RevenueCat cache hoặc kiểm tra trạng thái)
-setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
-
-// Trả lại headers đã sửa đổi
-$done({ headers: modifiedHeaders });
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
