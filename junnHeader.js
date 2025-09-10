bc7d1a05293ac91bd9f5d11918951b353d53b0ee
junn-dev <hungnguyen.junn@gmail.com>
2025-09-11 03:19:34 +0700

    1.0.1

--- junnHeader.js
+++ junnHeader.js
@@ -4,5 +4,22 @@
 
 const version = 'V1.0.2';
 
+/**
+ * Hàm setHeaderValue
+ * - e: object chứa headers
+ * - a: tên header
+ * - d: giá trị header cần set
+ */
+function setHeaderValue(e, a, d) {
+  var r = a.toLowerCase();
+  r in e ? e[r] = d : e[a] = d;
+}
 
-function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
+// Lấy header gốc của request
+var modifiedHeaders = $request.headers;
+
+// Xóa giá trị của header X-RevenueCat-ETag
+setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
+
+// Trả lại headers đã chỉnh sửa
+$done({ headers: modifiedHeaders });
