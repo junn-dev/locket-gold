1dab40a838bfa1e5a93cf8db1113bfe9863b8e89
junn-dev <hungnguyen.junn@gmail.com>
2025-09-11 10:02:05 +0700

    1.0.3

--- junnHeader.js
+++ junnHeader.js
@@ -2,7 +2,28 @@
 > deleteHeader by Junn.k6
 ***********************************************/
 
-const version = 'V1.0.2';
+const version = 'V1.0.3';
 
+/**
+ * Hàm đặt lại giá trị header
+ * @param {object} headers - Object chứa toàn bộ headers
+ * @param {string} key - Tên header cần thay đổi
+ * @param {string} value - Giá trị mới
+ */
+function setHeaderValue(headers, key, value) {
+  const lowerKey = key.toLowerCase();
+  if (lowerKey in headers) {
+    headers[lowerKey] = value;
+  } else {
+    headers[key] = value;
+  }
+}
 
-function setHeaderValue(e, a, d) { var r = a.toLowerCase(); r in e ? e[r] = d : e[a] = d } var modifiedHeaders = $request.headers; setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", ""), $done({ headers: modifiedHeaders });
+// Lấy headers của request
+let modifiedHeaders = $request.headers;
+
+// Xóa giá trị "X-RevenueCat-ETag"
+setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
+
+// Trả lại request đã chỉnh sửa
+$done({ headers: modifiedHeaders });
